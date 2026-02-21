2ee0bcdbffe8e54879e534fb2080dd409fdaa954
maneesha465 <maneesha011235@gmail.com>
2026-02-21 15:25:15 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Education from "./components/Education";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 
